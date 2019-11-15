8545ba0ff6177aca1dde323320ef1ab497c3e307
MishaDeineko1990 <deinekomisha1990@gmail.com>
2019-11-15 20:29:04 +0200

    Train_002_upd_001

--- ruby/Train ruby/002.rb
+++ ruby/Train ruby/002.rb
@@ -3,16 +3,25 @@ def longest_consec(strarr, k)
     # your code
     n=strarr.length
     str = ""
+    n_str = 0
 
     def position_bigest_ell_arr(strarr)
-        
+        strarr.length.times do |i|
+            if strarr[i].length>n_str 
+                n_str=strarr[i].length
+            end
+        end
     end
+
+    position_bigest_ell_arr(strarr)
+
+    puts n_str
 
     if k>n || n==0
         # return str
         puts "str"
     else
-        puts "blabla"
+        
     end
 end
 
