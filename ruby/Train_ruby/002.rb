e6f14d699cb8774fe4594d04e134ca49cdb501a3
MishaDeineko1990 <deinekomisha1990@gmail.com>
2019-11-21 19:03:53 +0200

    tarin_002_update_004

--- ruby/Train_ruby/002.rb
+++ ruby/Train_ruby/002.rb
@@ -1,40 +1,26 @@
-
 def longest_consec(strarr, k)
     # your code
     n=strarr.length
     str = ""
-    n_str = 0
-    
-    
-    def position_bigest_ell_arr(strarr)
-        puy =0
-        strarr.length.times do |i|
-            # puts strarr[i].length
-            lng = strarr[i].length
-            
-            if puy<lng
-                puy=lng
-                
-            end
-        end
-        puts puy
-        return n_str=puy
-    end
-    position_bigest_ell_arr(strarr)
-    
-    
-    def worck_fom_arr(k, n, n_str)
-        
-        if k>n || n==0
-            # return str
-            puts "str"
-        else
-            puts n_str        
-        end
-    end
-    worck_fom_arr()
+    @leng_el = 0
+    @positon_bigget_el = 0
 
+
+    if k>n || n==0 
+        puts 'stddr'
+    else
+        strarr.each do |str_el, i|
+          if str_el.length > @leng_el
+               @positon_bigget_el = i
+               @leng_el = str_el.length
+               puts i.to_s
+          end
+        end
+        
+        puts "fuckS #{@positon_bigget_el}"
+    end
 end
 
 
-longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2)
+
+longest_consec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 1)
